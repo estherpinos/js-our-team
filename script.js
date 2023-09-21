9f02952ffccd940e3fb04f77fb17306ea89a1325
estherpinos <estherpinos1@gmail.com>
2023-09-21 14:11:48 +0200

    Add [files and array infoPersone]

---
+++ script.js
@@ -0,0 +1,39 @@
+// 1.Creo array con i dati
+
+const infoPersone= [
+
+  {
+    nome: 'Wayne Barnett',
+    ruolo:'Founder & CEO',
+    image:'wayne-barnett-founder-ceo.jpg'
+  },
+
+  {
+    nome:'Angela Caroll',
+    ruolo:'	Chief Editor',
+    image:'	angela-caroll-chief-editor.jpg'
+  },
+
+  {
+    nome:'Walter Gordon',
+    ruolo:'	Office Manager',	image:'walter-gordon-office-manager.jpg'
+  },
+
+  {
+    nome:'Angela Lopez',
+    ruolo:'Social Media Manager',	
+    image:'angela-lopez-social-media-manager.jp'
+  },
+
+  {
+    nome:'Scott Estrada',
+    ruolo:'	Developer',
+    image:'	scott-estrada-developer.jpg'
+  },
+
+  {
+    nome:'Barbara Ramos'	,
+    ruolo:'Graphic Designer',
+    image:'barbara-ramos-graphic-designer.jpg'
+  }
+]
